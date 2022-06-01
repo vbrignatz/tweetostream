d5867b6aa6bdcc96c7aba4ae89c12ebdb7b961fe
vbrignatz <vincentbrignatz@gmail.com>
2022-06-01 10:34:09 +0200

    [up] added argument parsing

--- consumer.py
+++ consumer.py
@@ -1,18 +1,27 @@
 from scorer import Scorer
 
 import json
+import argparse
 
 from kafka import KafkaConsumer
 from pymongo import MongoClient
 
+parser = argparse.ArgumentParser(description='Fetch some tweets and upload them in kafka')
+parser.add_argument('--kafkaport', type=int, default=9092, help="Kafka port")
+parser.add_argument('--kafkahost', type=str, default="localhost", help="Kafka hostname")
+parser.add_argument('--mongohost', type=str, default="localhost", help="Mongo hostname")
+parser.add_argument('--mongoport', type=int, default=27017, help="Mongo port")
+parser.add_argument('-t', '--topic', type=str, default="twitto", help="The name of the topic. Carefull, this should be the same in producer.py")
+args = parser.parse_args()
+
 # Connect to MongoDB
-mongoclient = MongoClient(port=27017)
+mongoclient = MongoClient(host=[f'{args.mongohost}:{args.mongoport}'])
 db = mongoclient.twitto
 
 # Setup Kafka consumer
 consumer = KafkaConsumer(
-    'twitto',
-    bootstrap_servers=['localhost:9092'],
+    args.topic,
+    bootstrap_servers=[f'{args.kafkahost}:{args.kafkaport}'],
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
     )
 
