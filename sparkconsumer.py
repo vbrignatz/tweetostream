9d73072f58abe7c0231e578989d59c1e7bbc929f
moons54 <aurelienmimouni@gmail.com>
2022-06-15 10:40:54 +0200

    'modification argument sparkconsumer'

--- sparkconsumer.py
+++ sparkconsumer.py
@@ -1,55 +1,74 @@
+
+from operator import contains
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from pyspark.sql.types import StringType, StructType
+from pyspark.sql.types import StringType, StructType, DateType, StructField, IntegerType, FloatType
 from pyspark.sql.types import *
 from pyspark.sql.functions import col, split
 from pyspark.sql import functions as F
 from scorer import Scorer, getscore
 from pymongo import MongoClient
 import argparse
-from time import sleep
+import json
+
+mongoclient = MongoClient(port=27017)
+db = mongoclient.twitto
+
+
 
 # Argument parsing
-parser = argparse.ArgumentParser(description='Fetch some tweets from kafka')
-parser.add_argument('--kafkaport', type=int, default=9092, help="Kafka port")
-parser.add_argument('--kafkahost', type=str, default="localhost", help="Kafka hostname")
-parser.add_argument('--mongohost', type=str, default="localhost", help="Mongo hostname")
-parser.add_argument('--mongoport', type=int, default=27017, help="Mongo port")
+parser = argparse.ArgumentParser(description='Fetch some tweets and upload them in spark')
+parser.add_argument('--sparkport', type=int, default=9092, help="Kafka port")
+parser.add_argument('--sparkhost', type=str, default="localhost", help="Kafka hostname")
 parser.add_argument('-t', '--topic', type=str, default="twitto", help="The name of the topic. Carefull, this should be the same in producer.py")
+parser.add_argument('--fields', type=str, default="", help="thematic")
 args = parser.parse_args()
 
-# TODO: find a better solution
-SLEEP_TIME = 10
-print(f"Waiting {SLEEP_TIME}s for services to start...")
-sleep(SLEEP_TIME)
-print("Starting ...")
-
-spark = SparkSession.\
-        builder.\
-        appName("Sparktostream").\
-        config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector:10.0.0').\
-        config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0').\
-        getOrCreate()
+def process(batch_df,batch_id):
+    
+    for col in batch_df.collect():
+        
+        
+        result = db.test.insert_one({'text':col['text'],'score':col['score']})
+        print(f'Inserted {result.inserted_id} with score {result}')
+    pass
 
 
+# mise en place de la session en chargeant les drivers 
+spark = SparkSession \
+    .builder \
+    .appName("TwitterSentimentAnalysis") \
+    .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector:10.0.0') \
+    .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0') \
+    .getOrCreate()
 
+
+# nous modifions le niveau de log a error
 spark.sparkContext.setLogLevel("ERROR")
+
+# ouverture dun flux vers kafka server 
+# en entrée nous indiquons host port et topic
 
 df = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers",f"{args.kafkahost}:{args.kafkaport}") \
+    .option("kafka.bootstrap.servers",f"{args.sparkhost}:{args.sparkport}") \
     .option("subscribe", f"{args.topic}") \
     .load()
 
-sc = Scorer()
+
+#sc = Scorer()
+#Instanciation de la methode getscore et on le parse en float
 score = F.udf(getscore, FloatType())
 
+#Recuppération de des valeurs en string  
 tweet_df_string = df.selectExpr("CAST(value AS STRING)")
 
+# dans notre 
 twt = StructType() \
     .add("text",StringType()) \
-    .add("created_at",DateType())
+    .add("created_at",DateType()) \
+    .add("entities",StringType())
 
 
 values = df.select(from_json(df.value.cast("string"), twt).alias("tweet"))
@@ -59,16 +78,15 @@ df1 = values.select("tweet.*")
 
 order3 = df1 \
     .withColumn("score",score(col("text")))
-#obtenir une sortie d'ecran des stream
-twt1 = order3\
-    .writeStream \
-    .outputMode("update") \
-    .option("truncate","false") \
-    .format("console") \
-    .start()
 
-twt1.awaitTermination()
 
+
+#dsw = (order3.writeStream.format("mongodb").queryName("ToMDB").option('spark.mongodb.connection.uri', 'mongodb+srv://moons:mongomoons@cluster0.eky2uou.mongodb.net/?retryWrites=true&w=majority').option('spark.mongodb.database', 'twitto').option('spark.mongodb.collection', 'test').trigger(continuous="10 seconds").outputMode("append").start().awaitTermination());
+
+query = order3.writeStream.queryName("test_tweets") \
+        .foreachBatch(process).start()
+query.awaitTermination()
 
 
 print("----- streaming is running -------")
+
